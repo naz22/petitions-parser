43e84c9090163605bdf0e8f3c880a83924628513
Anas Adurker <anas.adurker@hotmail.co.uk>
2019-04-01 23:54:08 +0100

    using multiple assignment for variables

--- JsonReader.py
+++ JsonReader.py
@@ -2,15 +2,12 @@ import json
 
 with open("data/241584.json") as json_data:
     data = json.load(json_data)
-    constitution = "Constitution"
-    mp = "Member of Parliament"
-    sig_count = "Signature Count"
-    breakp = "||"
-    print("%-45s %-3s %-35s %-3s %-20s %-3s" % (constitution, breakp, mp, breakp, sig_count, breakp))
-    print("-" * 113)
+    constitution, mp, sig_count, breakpoints, sep = "Constitution", "Member of Parliament", "Signature Count", "||", \
+                                                    "-" * 113
+    print("%-45s %-3s %-35s %-3s %-20s %-3s" % (constitution, breakpoints, mp, breakpoints, sig_count, breakpoints))
+    print(sep)
     for r in data['data']['attributes']['signatures_by_constituency']:
-        constitution_id = r['name']
-        mp_id = r['mp']
-        sig_count_id = r['signature_count']
-        print("%-45s %-3s %-35s %-3s %-20s %-3s" % (constitution_id, breakp, mp_id, breakp, sig_count_id, breakp))
-    print("-" * 113)
+        constitution_id, mp_id, sig_count_id = r['name'], r['mp'], r['signature_count']
+        print("%-45s %-3s %-35s %-3s %-20s %-3s" % (constitution_id, breakpoints, mp_id, breakpoints, sig_count_id,
+                                                    breakpoints))
+    print(sep)
