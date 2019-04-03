14299959b3f029ac2d60c619bd4b19e843c00e2f
Anas Adurker <anas.adurker@hotmail.co.uk>
2019-04-03 14:19:09 +0100

    fixed encoding bug which outputted JSON data 'Ynys Môn' as 'Ynys MÃ´n'

--- JsonReader.py
+++ JsonReader.py
@@ -1,12 +1,14 @@
-import json
+import simplejson
 
-with open("data/241584.json") as json_data:
-    data = json.load(json_data)
+with open("data/241584.json", encoding='utf-8') as json_data:
+    data = simplejson.load(json_data)
     constitution, mp, sig_count, breakpoints, sep = "Constitution", "Member of Parliament", "Signature Count", "||", \
                                                     "-" * 113
     print(f'Petition ' + str(data['data']['id']) + ': ' + data['data']['attributes']['action'])
+    print(sep)
     print("%-45s %-3s %-35s %-3s %-20s %-3s" % (constitution, breakpoints, mp, breakpoints, sig_count, breakpoints))
     print(sep)
+
     for r in data['data']['attributes']['signatures_by_constituency']:
         constitution_id, mp_id, sig_count_id = r['name'], r['mp'], r['signature_count']
         print("%-45s %-3s %-35s %-3s %-20s %-3s" % (constitution_id, breakpoints, mp_id, breakpoints, sig_count_id,
